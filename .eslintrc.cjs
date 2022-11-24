d432c5ba668e5e325f2572b15cf13fc467d6ffd7
吴磊 <wulei@santu.com>
2022-11-24 15:00:08 +0800

    [功能]：增加自动更新package.json中的version版本号脚本

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
     'no-console': [
       'error',
       {
-        allow: ['warn', 'error'],
+        allow: ['warn', 'error', 'info'],
       },
     ],
     'padding-line-between-statements': [
