7584353cfb8b332a015cee913321fad04c8a884b
吴磊 <wulei0407@163.com>
2023-03-07 10:12:54 +0800

    [功能]：添加pinia日志打印插件

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
-import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import * as VueRouter from 'vue-router';
 import App from './App.vue';
 import { routes } from './common/common-router';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import { pinia } from './stores';
 import 'dayjs/locale/zh-cn';
 import 'animate.css';
 import './assets/css/normalize.scss';
 import './common/common-set-rem';
-import { VueQueryPlugin } from '@tanstack/vue-query';
 
 const app = createApp(App);
-
-// 创建全局状态
-// !全局状态日志查看：安装Vue浏览器插件后找到Pinia模块可查看所有状态
-const pinia = createPinia();
 
 // 路由
 const router = VueRouter.createRouter({
@@ -21,7 +17,7 @@ const router = VueRouter.createRouter({
   routes,
 });
 
-app.use(pinia);
 app.use(router);
 app.use(VueQueryPlugin);
+app.use(pinia);
 app.mount('#app');
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,24 @@
 /**
  * stores状态模块化
  */
+import { createPinia } from 'pinia';
 import useUserStore from './storeUser.js';
+import PiniaLogger from 'pinia-logger';
+
+// 创建全局状态
+export const pinia = createPinia();
+
+// 全局状态日志查看
+pinia.use(
+  PiniaLogger({
+    expanded: true,
+    disabled: import.meta.env.MODE === 'production',
+    // 使用筛选器只记录特定的操作
+    // filter: ({ name }) => name !== 'incrementCounter',
+    // 如果未定义，所有操作都将被记录
+    // actions: [],
+  }),
+);
 
 export default function useStore() {
   return {
