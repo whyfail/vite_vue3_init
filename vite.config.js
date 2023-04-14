0386771bcc9d0ae628a4cc0c149ad51858e3244a
吴磊 <wulei0407@163.com>
2023-04-14 09:10:56 +0800

    [功能]:将压缩代码模式更改为terser,并打包剔除console和debugger

--- vite.config.js
+++ vite.config.js
@@ -87,5 +87,12 @@ export default defineConfig({
         },
       },
     },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
 });
