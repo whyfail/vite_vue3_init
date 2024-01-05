3580d27f108edc07d9b46c927009e4f0a4e3b969
吴磊 <wulei0407@163.com>
2024-01-05 18:30:37 +0800

    功能：添加时间函数

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,20 +1,38 @@
 /**
  * token管理
  */
+
+/** token 存储key */
 const KEY_TOKEN = 'xxx_web_app_token';
 
+/**
+ * 是否登录
+ * @returns {boolean}
+ */
 const isLogin = () => {
   return !!localStorage.getItem(KEY_TOKEN);
 };
 
+/**
+ * 获取token
+ * @returns {string}
+ */
 const getToken = () => {
   return localStorage.getItem(KEY_TOKEN);
 };
 
+/**
+ * 设置token
+ * @returns {void}
+ */
 const setToken = (token) => {
   localStorage.setItem(KEY_TOKEN, token);
 };
 
+/**
+ * 清除token
+ * @returns {void}
+ */
 const clearToken = () => {
   localStorage.removeItem(KEY_TOKEN);
 };
