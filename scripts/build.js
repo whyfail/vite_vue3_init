b61efa777df7b9d13b0441343f3213352e6c86d6
吴磊 <wulei0407@163.com>
2024-09-05 09:30:50 +0800

    功能：去掉打包自动升级版本号功能，以免影响自动化构建

--- scripts/build.js
+++ scripts/build.js
@@ -37,32 +37,6 @@ if (!isGitInitialized()) {
     }
   }
 
-  /**
-   * 自动升级package.json中的version小本版号
-   */
-  const packageJsonStr = fs.readFileSync('./package.json').toString();
-
-  try {
-    const packageJson = JSON.parse(packageJsonStr);
-    // 升级版本号
-    const arr = packageJson.version.split('.');
-
-    arr[2] = +arr[2] + 1;
-
-    const newVersion = arr.join('.');
-
-    packageJson.version = newVersion;
-
-    console.info('版本升级为' + newVersion);
-
-    fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, '\t'));
-
-    execSync(`git add package.json`);
-  } catch (e) {
-    console.error('处理package.json失败，请重试', e.message);
-    process.exit(1);
-  }
-
   try {
     execSync('vite build', { stdio: 'inherit' });
   } catch (error) {
