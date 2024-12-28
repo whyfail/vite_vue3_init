6829c2dc96308d37bf8129b65e5163a7f081276b
吴磊 <wulei0407@163.com>
2024-12-28 11:36:01 +0800

    修复：格式化

--- src/pages/module-login/LoginForm.vue
+++ src/pages/module-login/LoginForm.vue
@@ -1,6 +1,37 @@
+<script setup>
+import { setToken } from '@/utils/auth.js';
+import { Lock, User } from '@element-plus/icons-vue';
+import { ElButton, ElCheckbox, ElForm, ElFormItem, ElInput, ElMessage } from 'element-plus';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const FORM_RULES = {
+  account: [{ required: true, message: '账号必填', type: 'error' }],
+  password: [{ required: true, message: '密码必填', type: 'error' }],
+};
+
+const type = ref('password');
+
+const form = ref();
+const formData = ref({
+  account: 'admin',
+  password: 'admin',
+  checked: false,
+});
+
+function goHome() {
+  ElMessage.success('登录成功');
+  router.replace('/home');
+
+  setToken('123', formData.value.checked);
+}
+</script>
+
 <template>
-  <ElForm ref="form" class="mt-40px w-400px" :data="formData" :rules="FORM_RULES">
-    <template v-if="type == 'password'">
+  <ElForm ref="form" class="mt-[40px] w-[400px]" :data="formData" :rules="FORM_RULES">
+    <template v-if="type === 'password'">
       <ElFormItem name="account">
         <ElInput v-model="formData.account" size="large" placeholder="请输入账号：admin" :prefix-icon="User" />
       </ElFormItem>
@@ -18,43 +49,16 @@
       </ElFormItem>
 
       <ElFormItem>
-        <ElButton class="w-100%" type="primary" size="large" @click="goHome">登录</ElButton>
+        <ElButton class="w-[100%]" type="primary" size="large" @click="goHome">
+          登录
+        </ElButton>
       </ElFormItem>
 
-      <div class="mb-16px flex items-center justify-between">
-        <ElCheckbox v-model="formData.checked">记住账号</ElCheckbox>
+      <div class="mb-[16px] flex items-center justify-between">
+        <ElCheckbox v-model="formData.checked">
+          记住账号
+        </ElCheckbox>
       </div>
     </template>
   </ElForm>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { User, Lock } from '@element-plus/icons-vue';
-import { ElButton, ElCheckbox, ElForm, ElFormItem, ElInput, ElMessage } from 'element-plus';
-import { setToken } from '@/utils/auth.js';
-
-const router = useRouter();
-
-const goHome = () => {
-  ElMessage.success('登录成功');
-  router.replace('/home');
-
-  setToken('123', formData.value.checked);
-};
-
-const FORM_RULES = {
-  account: [{ required: true, message: '账号必填', type: 'error' }],
-  password: [{ required: true, message: '密码必填', type: 'error' }],
-};
-
-const type = ref('password');
-
-const form = ref();
-const formData = ref({
-  account: 'admin',
-  password: 'admin',
-  checked: false,
-});
-</script>
