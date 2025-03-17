808af576ebc784726ecfc361f57610729b78d07b
吴磊 <wulei0407@163.com>
2025-03-17 18:20:38 +0800

    更新：更新eslint配置以校验css、html、markdown文件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,24 @@ export default antfu(
     vue: true,
     unocss: true,
     typescript: false,
+    formatters: {
+      /**
+       * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+       * By default uses Prettier
+       */
+      css: true,
+      /**
+       * Format HTML files
+       * By default uses Prettier
+       */
+      html: true,
+      /**
+       * Format Markdown files
+       * Supports Prettier and dprint
+       * By default uses Prettier
+       */
+      markdown: 'prettier',
+    },
   },
   {
     rules: {
