b859761f47c121fb41f9f9b13867b3697b0dd5b1
吴磊 <wulei0407@163.com>
2025-12-19 09:48:35 +0800

    refactor(stores): 重构用户存储并移除公共存储
    将storeUser从options API迁移至composition API，并移除不再使用的storeCommon

--- src/stores/storeUser.js
+++ src/stores/storeUser.js
@@ -1,20 +1,30 @@
 import { defineStore } from 'pinia';
 
-const useUserStore = defineStore('storeOther', {
-  state: () => ({
-    number: 0,
-    role: 'admin',
-  }),
+const useUserStore = defineStore('storeUser', () => {
+  const number = ref(0);
+  const role = ref('admin');
 
-  actions: {
-    addNumber() {
-      this.number += 1;
-    },
-    subtractNumber() {
-      this.number -= 1;
-    },
-  },
-  // 持久化
+  /**
+   * 增加number
+   */
+  function addNumber() {
+    number.value += 1;
+  }
+
+  /**
+   * 减少number
+   */
+  function subtractNumber() {
+    number.value -= 1;
+  }
+
+  return {
+    number,
+    role,
+    addNumber,
+    subtractNumber,
+  };
+}, {
   persist: true,
 });
 
